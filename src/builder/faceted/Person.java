ed485e5180e32688a6584d5310f6b95a4ac421bb
Mohammad Darwish <mohammad.darwish@ericsson.com>
2022-11-09 16:49:37 +0100

    Faceted Builder DP

---
+++ src/builder/faceted/Person.java
@@ -0,0 +1,22 @@
+package builder.faceted;
+
+public class Person {
+    //Address
+    public String streetAddress, postcode, city;
+
+    // Employee
+    public String companyName, position;
+    public int annualIncome;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+            "streetAddress='" + streetAddress + '\'' +
+            ", postcode='" + postcode + '\'' +
+            ", city='" + city + '\'' +
+            ", companyName='" + companyName + '\'' +
+            ", position='" + position + '\'' +
+            ", annualIncome=" + annualIncome +
+            '}';
+    }
+}
