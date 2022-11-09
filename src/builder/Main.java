a9a25301783c7a1983373f2a062255fc18e93e7e
Mohammad Darwish <mohammad.darwish@ericsson.com>
2022-11-09 15:31:54 +0100

    fluent builder DP

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -3,8 +3,7 @@ package builder;
 public class Main {
     public static void main(String[] args) {
         HtmlBuilder builder = new HtmlBuilder("url");
-        builder.addChild("li", "Hello");
-        builder.addChild("li", "World");
+        builder.addChild("li", "Hello").addChild("li", "World");
         System.out.println(builder);
     }
 }
