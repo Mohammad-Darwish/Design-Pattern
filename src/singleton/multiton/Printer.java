7794ab5b605cff83630d39b864f654045f7ffdd2
Mohammad Darwish <mohammad.darwish@ericsson.com>
2022-11-15 13:35:56 +0100

    Multiton Singleton

---
+++ src/singleton/multiton/Printer.java
@@ -0,0 +1,27 @@
+package singleton.multiton;
+
+import java.util.HashMap;
+
+/**
+ * Lazy Initialization
+ * Has three instances maximum.
+ */
+public class Printer {
+    private static HashMap<Subsystem, Printer> instances = new HashMap<>();
+    private static int instancesCount = 0;
+
+    private Printer() {
+        instancesCount++;
+        System.out.println("A total of " +
+            instancesCount + " instances created so far.");
+    }
+
+    public static Printer get(Subsystem sys) {
+        if (instances.containsKey(sys)) {
+            return instances.get(sys);
+        }
+        Printer instance = new Printer();
+        instances.put(sys, instance);
+        return instance;
+    }
+}
