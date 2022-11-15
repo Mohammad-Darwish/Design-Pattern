ec6ea21aff0a224bc97a7889a513fd0a864c5f67
Mohammad Darwish <mohammad.darwish@ericsson.com>
2022-11-15 13:21:29 +0100

    Monostate Singleton

---
+++ src/singleton/monostate/Demo.java
@@ -0,0 +1,11 @@
+package singleton.monostate;
+
+public class Demo {
+    public static void main(String[] args) {
+        ChiefExecutiveOfficer ceo = new ChiefExecutiveOfficer();
+        ceo.setName("Moh");
+        ceo.setAge(26);
+        ChiefExecutiveOfficer ceo2 = new ChiefExecutiveOfficer();
+        System.out.println(ceo);
+    }
+}
