1f886551548101ec4f9827277df5522e33acf3d8
Mohammad Darwish <mohammad.darwish@ericsson.com>
2022-11-19 12:31:16 +0100

    assignment Singleton

---
+++ src/singleton/assignment/SingletonTester.java
@@ -0,0 +1,10 @@
+package singleton.assignment;
+
+import java.util.function.Supplier;
+
+public class SingletonTester {
+    public static boolean isSingleton(Supplier<Object> func) {
+        // todo
+        return false;
+    }
+}
