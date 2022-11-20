515499fe53948b87738c3640edc2bbb99cac1ae1
Mohammad Darwish <mohammad.darwish@ericsson.com>
2022-11-20 22:12:56 +0100

    Bridge

---
+++ src/structural/bridge/Demo.java
@@ -0,0 +1,12 @@
+package structural.bridge;
+
+public class Demo {
+    public static void main(String[] args) {
+        RasterRenderer rasterRenderer = new RasterRenderer();
+        VectorRenderer vectorRenderer = new VectorRenderer();
+        Circle circle = new Circle(vectorRenderer, 5);
+        circle.draw();
+        circle.resize(2);
+        circle.draw();
+    }
+}
